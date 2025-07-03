edfcf05091ba33f05f162a80ca5c93e8b0f04171
node-23 <matheus.dev@hotmail.com>
2025-07-03 18:50:49 -0300

    Sale invoice added

--- src/app/items/sales-table/sales-table.component.ts
+++ src/app/items/sales-table/sales-table.component.ts
@@ -1,8 +1,9 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule, CurrencyPipe, DatePipe, NgForOf, NgIf} from "@angular/common";
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import {AuthService} from '../../auth.service';
 
 @Component({
   selector: 'app-sales-table',
@@ -24,16 +25,27 @@ export class SalesTableComponent {
   @Input() data: any[] = [];
   @Output() onDeleteSale = new EventEmitter<string>();
 
-  private readonly invoiceUrl = 'https://mogzipvuuf.execute-api.us-east-1.amazonaws.com/tcc/invoice';
-  private http = inject(HttpClient);
-  private snackBar = inject(MatSnackBar);
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    private authService: AuthService
+  ) {}
 
+  private readonly invoiceUrl = 'http://localhost:8080/api/sales/pdf';
   deleteSale(salesId: string): void {
     this.onDeleteSale.emit(salesId);
   }
 
   getInvoice(sale: any): void {
-    this.http.post(this.invoiceUrl, sale, { responseType: 'blob' }).subscribe({
+    const token = this.authService.getToken();
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', `Bearer ${token}`);
+    } else {
+      return;
+    }
+
+    this.http.get(`${this.invoiceUrl}/${sale.id}`, { headers: headers, responseType: 'blob', withCredentials: true }).subscribe({
       next: (response) => {
         const file = new Blob([response], { type: 'application/pdf' });
         const fileURL = URL.createObjectURL(file);
